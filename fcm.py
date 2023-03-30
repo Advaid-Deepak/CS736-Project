c67ad97f15fdc47a63c9795293cb4072f2d35a67
sanchit1053 <sanchit1053@gmail.com>
2023-03-30 15:43:52 +0530

    update memberships

--- fcm.py
+++ fcm.py
@@ -3,18 +3,46 @@ from matplotlib import pyplot as plt
 import numpy as np
 import cv2
 
-data_dict = mat73.loadmat('assignmentSegmentBrain.mat')
+def update_memberships(neighbourhood, pixels, centers, segments, q):
+    """ Return the new memberships assuming the centers
 
-image = data_dict["imageData"]
-image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    Args:
+        neighbourhood (The Neighbourhood defined bythe Gauusian): 
+        image (The Image given): 
+        centers (THe Centers provided by K-means): 
+        segments (Number of segments): 
+        q (The Fuzzy number): 
+    """
+
+    M = image.size
+
+    distance = np.zeros(M, segments)
+    for i in range(segments):
+        distance[:, i] = image**2  - 2 * centers[i] * image + centers[i] ** 2
+
+    power = 1 / (q - 1)
+    reverse_d = ( 1 / distance ) ** (power) 
+    sumD = np.sum(reverse_d, axis = 1)
+
+    memberships = np.zeros(M, segments)
+
+    for i in range(segments):
+        memberships[:, i] = reverse_d[:, i] / sumD
+     
+    return memberships
+
+data_dict = mat73.loadmat('assignmentsegmentbrain.mat')
+
+image = data_dict["imagedata"]
+image = cv2.cvtcolor(image, cv2.color_rgb2gray)
 image = (image * 256).astype(np.uint8)
-imageMask = data_dict["imageMask"]
+imagemask = data_dict["imagemask"]
 
 k = 4 
 
 
 pixels = np.float32(image.reshape((-1,1)))
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.85)
+criteria = (cv2.term_criteria_eps + cv2.term_criteria_max_iter, 100, 0.85)
 retval, labels, centers = cv2.kmeans(pixels, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
 
 centers = np.uint8(centers)
