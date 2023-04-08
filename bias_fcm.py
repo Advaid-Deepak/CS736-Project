5a9ee358294ddededbc676bc0aab12eb751486e5
sanchit1053 <sanchit1053@gmail.com>
2023-04-08 20:56:35 +0530

    working

--- bias_fcm.py
+++ bias_fcm.py
@@ -30,7 +30,7 @@ def J_fun(n, m, memberships,pixels,neighbourhood, bias, q, segments,centers):
 
 
 
-def class_means(n, m, memberships,pixels,neighbourhood, bias, q, segments) :
+def class_means(n, m, memberships,pixels,neighbourhood, bias, q, segments, imageMask) :
 
     numer = convolve2d(bias.reshape((n, m)), neighbourhood, "same").reshape(pixels.shape)
     denom = convolve2d(bias.reshape((n, m)) ** 2, neighbourhood, "same").reshape(pixels.shape)
@@ -40,8 +40,8 @@ def class_means(n, m, memberships,pixels,neighbourhood, bias, q, segments) :
     for k in range(segments):
         powered_Membership = memberships[:, k] ** q
         powered_Membership = powered_Membership.reshape((-1,1))
-        num = sum(powered_Membership * pixels * numer)
-        den = sum(powered_Membership * denom)
+        num = sum(powered_Membership * (pixels * numer * imageMask))
+        den = sum(powered_Membership * (denom * imageMask))
         centers[k] = num / den
     return centers
 
@@ -54,7 +54,7 @@ def gauss(size, sigma = 0.5):
         h = h / s
     return h
 
-def update_bias(n, m, neighbourhood, pixels, memberships, centers, segments, q):
+def update_bias(n, m, neighbourhood, pixels, memberships, centers, segments, q, imageMask):
     
     numer = np.zeros (pixels.shape)
     denom = np.zeros (pixels.shape)
@@ -72,6 +72,7 @@ def update_bias(n, m, neighbourhood, pixels, memberships, centers, segments, q):
     bias[np.isnan(bias)] = 0
     bias[np.isinf(bias)] = 0
 
+    bias = bias * imageMask
     return bias
     
 
@@ -100,20 +101,26 @@ def update_memberships(n, m, neighbourhood, pixels, centers, bias, segments, q, 
     for i in range(segments):
         distance[:, i] = ((pixels**2) * w  - 2 * (centers[i] * pixels)*t1  + (centers[i] ** 2)*t2 ).flatten()
 
+    distance = distance * imageMask
+
     distance[distance < 0] = 0
-    p = 1 / (q - 1)
+    p =  1 / (q - 1)
     reverse_d = ( 1 / distance ) ** (p) 
     sumD = np.nansum(reverse_d, axis = 1).reshape((-1,1))
 
     memberships = np.zeros((M, segments))
 
-    for i in range(segments):
-        temp = reverse_d[:, i].reshape((-1, 1))
-        temp = temp / sumD
-        temp[imageMask == 0] = 0
-        memberships[:, i] = temp.reshape((-1))
+    memberships = reverse_d / sumD
+
+    # for i in range(segments):
+    #     temp = reverse_d[:, i].reshape((-1, 1))
+    #     temp = temp / sumD
+    #     temp[imageMask == 0] = 0
+    #     memberships[:, i] = temp.reshape((-1))
     memberships[np.isnan(memberships)] = 0
     memberships[np.isinf(memberships)] = 0
+
+    memberships = memberships * imageMask
     return memberships
 
 
@@ -132,7 +139,7 @@ imagemask = imagemask.reshape((-1, 1))
 
 
 k = 3
-q = 1.6
+q = 1.1
 
 pixels = np.float32(image.reshape((-1,1)))
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.85)
@@ -144,8 +151,10 @@ uInit = np.zeros((pixels.shape[0],centers.shape[0]))
 for i in range(pixels.shape[0]):
     uInit[i,labels[i]] = 1
 
-bInit = np.ones(pixels.shape)
+
+bInit = np.ones(pixels.shape) * imagemask
 neighbourhood = gauss(10)
+print(neighbourhood)
 
 maxIters = 20 
 u = uInit
@@ -154,32 +163,35 @@ bias = bInit
 
 for i in range(maxIters):
     u = update_memberships(image_rows, image_cols, neighbourhood,pixels,centers,bias, k,q, imagemask)
-    centers = class_means(image_rows, image_cols, u,pixels,neighbourhood, bias, q, k)
-    bias = update_bias(image_rows, image_cols, neighbourhood, pixels, u, centers, k, q)
-    bias[imagemask == 0] = 0
+    centers = class_means(image_rows, image_cols, u,pixels,neighbourhood, bias, q, k, imagemask)
+    bias = update_bias(image_rows, image_cols, neighbourhood, pixels, u, centers, k, q, imagemask)
     J = J_fun(image_rows, image_cols, u,pixels,neighbourhood, bias, q, k,centers)
     print(i,J)
     
-    labels = np.argmax(u,axis = 1)
-    intcenters = np.uint8(centers)
-    segmented_data = intcenters[labels.flatten()]
-    segmented_image = segmented_data.reshape((image.shape))
+labels = np.argmax(u,axis = 1)
+intcenters = np.uint8(centers)
+segmented_data = intcenters[labels.flatten()]
+segmented_image = segmented_data.reshape((image.shape))
 
-    savedCenters = np.uint8(savedCenters)
-    kmeans_segmented_data = savedCenters[savedLabels.flatten()]
-    kmeans_segmented_data = kmeans_segmented_data.reshape((image.shape))
+retval, labels, centers = cv2.kmeans(pixels, k+1, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+centers = np.uint8(centers)
+kmeans_segmented_data = centers[labels.flatten()]
+kmeans_segmented_data = kmeans_segmented_data.reshape((image.shape))
 
-    biasRemoved = np.zeros(pixels.shape)
-    for j in range(k):
-        biasRemoved = biasRemoved + u[:, j].reshape((-1,1)) * centers[j]
-    biasRemoved = biasRemoved * imagemask
+biasRemoved = np.zeros(pixels.shape)
+for j in range(k):
+    biasRemoved = biasRemoved + u[:, j].reshape((-1,1)) * centers[j]
+biasRemoved = biasRemoved * imagemask
 
-    fig, axs = plt.subplots(3, 2 )
-    axs[0][0].imshow(image,cmap='gray')
-    axs[0][1].imshow(segmented_image,cmap='gray')
-    axs[1][0].imshow(kmeans_segmented_data, cmap = 'gray')
-    axs[1][1].imshow(bias.reshape(image.shape), cmap = 'gray')
-    axs[2][0].imshow(biasRemoved.reshape(image.shape), cmap = 'gray')
-    
-    plt.show()
+
+fig, axs = plt.subplots(3, 2 )
+axs[0][0].imshow(image,cmap='gray')
+axs[0][1].imshow(segmented_image)
+axs[1][0].imshow(kmeans_segmented_data)
+axs[1][1].imshow(bias.reshape(image.shape), cmap = 'gray')
+axs[2][0].imshow(biasRemoved.reshape(image.shape), cmap = 'gray')
+axs[2][1].imshow((pixels - biasRemoved*bias).reshape(image.shape), cmap='gray')
+
+
+plt.show()
 
